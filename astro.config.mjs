64b417dcb8beee741954f44ebae97ea08e8ed971
Maurice Pasternak <maurice.pasternak@utoronto.ca>
2024-09-01 09:05:45 -0400

    Added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import { defineConfig } from 'astro/config'
 
 import alpinejs from '@astrojs/alpinejs'
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), icon(), alpinejs()],
-	// site: 'https://github.com/MauricePasternak/bpvisualarts.github.io',
-	// base: '/bpvisualarts',
-	site: 'https://bpvisualarts.ca',
+    integrations: [tailwind(), icon(), alpinejs(), sitemap()],
+    // site: 'https://github.com/MauricePasternak/bpvisualarts.github.io',
+    // base: '/bpvisualarts',
+    site: 'https://bpvisualarts.ca',
 })
